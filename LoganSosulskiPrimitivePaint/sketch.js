fd29948beb94e6ee4fba320d27f27c2341752cf5
logansosulski <47329522+logansosulski@users.noreply.github.com>
2019-02-14 10:35:24 -0600

    i9 finally didd it

--- LoganSosulskiPrimitivePaint/sketch.js
+++ LoganSosulskiPrimitivePaint/sketch.js
@@ -5,9 +5,14 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let shapeSize = 200;
-let dataArray = [];
+let shapeSize = 100;
+let dataArrayRect = [];
+let dataArrayEllipse = [];
+let dataArrayTri = [];
 let shapeType = "rect";
+let r = 255;
+let g = 0;
+let b = 255;
 
 
 function setup() {
@@ -16,7 +21,7 @@ function setup() {
 
 function draw() {
   background(0);
-  fill(random(255),random(255),random(255));
+  fill(r,g,b);
   if (shapeType === "rect") {
     rect(mouseX-shapeSize/2,mouseY-shapeSize/2,shapeSize,shapeSize);
   }
@@ -25,6 +30,10 @@ function draw() {
   }
   if (shapeType === "triangle") {
     triangle(mouseX-shapeSize/2,mouseY+shapeSize/2,mouseX+shapeSize/2,mouseY+shapeSize/2,mouseX,mouseY-shapeSize/2);
+  }
+  for (let i=0; i<dataArrayRect.length; i++) {
+    fill(dataArrayRect[i][3],dataArrayRect[i][4],dataArrayRect[i][5]);
+    rect(dataArrayRect[i][0], dataArrayRect[i][1], dataArrayRect[i][2], dataArrayRect[i][2]);
   }
 }
 
@@ -44,4 +53,34 @@ function keyTyped() {
   if (keyCode === 32) {
     background(0);
   }
+}
+
+function mouseWheel(event) {
+  if (event.delta > 0) {
+    size -= 50;
+  }
+  else {
+    size += 50;
+  }
+  if (size <= 50) {
+    size += 50;
+  }
+}
+  
+function mousePressed() {
+  r = random(255);
+  g = random(255);
+  b = random(255);
+  if (shapeType === "rect") {
+    let cur = [mouseX-shapeSize/2,mouseY-shapeSize/2,shapeSize,r,g,b];
+    dataArrayRect.push(cur);
+  }
+  if (shapeType === "ellipse") {
+    let cur = [mouseX,mouseY,shapeSize,r,g,b];
+    dataArrayEllipse.push(cur);
+  }
+}
+
+for (let i=0; i<dataArray.length; i++) {
+  image(img, dataArray[i][0], dataArray[i][1], dataArray[i][2], dataArray[i][2]);
 }
