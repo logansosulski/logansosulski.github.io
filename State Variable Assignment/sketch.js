b30c3aaa038647063718b50b2261914a40ebf28c
logansosulski <47329522+logansosulski@users.noreply.github.com>
2019-03-04 11:03:00 -0600

    ayo one more and you got it

--- State Variable Assignment/sketch.js
+++ State Variable Assignment/sketch.js
@@ -18,6 +18,7 @@ let b3 = 0;
 let r4 = 255;
 let g4 = 255;
 let b4 = 0;
+let light = "off";
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -26,49 +27,14 @@ function setup() {
 function draw() {
   background(255);
   quadrentLocation();
-  
-  
-
-  if (mouseQuad === 1) {
-    fadeIn(r1, g1, b1);
-  }
-  else {
-    fadeOut(r1, g1, b1);
-  }
-  fill(r1, g1, b1);
+  fade();
+  fill(r1,g1,b1);
   rect(width / 2, 0, width / 2, height / 2);
-
-
-
-  if (mouseQuad === 2) {
-    //fadeIn(r2,g2,b2);
-  }
-  else {
-    //fadeOut(r2,g2,b2);
-  }
-  fill(r2, g2, b2);
+  fill(r2,g2,b2);
   rect(0, 0, width / 2, height / 2);
-
-
-
-  if (mouseQuad === 3) {
-    //fadeIn(r3, g3, b3);
-  }
-  else {
-    //fadeOut(r3, g3, b3);
-  }
-  fill(r3, g3, b3);
+  fill(r3,g3,b3);
   rect(0, height / 2, width / 2, height / 2);
-
-
-
-  if (mouseQuad === 4) {
-    //fadeIn(r4, g4, b4);
-  }
-  else {
-    //fadeOut(r4, g4, b4);
-  }
-  fill(r4, g4, b4);
+  fill(r4,g4,b4);
   rect(width / 2, height / 2, width / 2, height / 2);
 }
 
@@ -80,10 +46,24 @@ function fade() {
       fill(r1,g1,b1);
     }
   }
+  else {
+    if (b1>0) {
+      b1=b1-15;
+      g1=g1+15;
+      fill(r1,g1,b1);
+    }
+  }
   if (mouseQuad === 2) {
     if (b2<255) {
       b2=b2+15;
       g2=g2-15;
+      fill(r2,g2,b2);
+    }
+  }
+  else {
+    if (b2>0) {
+      b2=b2-15;
+      g2=g2+15;
       fill(r2,g2,b2);
     }
   }
@@ -94,13 +74,41 @@ function fade() {
       fill(r3,g3,b3);
     }
   }
+  else {
+    if (b3>0) {
+      b3=b3-15;
+      g3=g3+15;
+      fill(r3,g3,b3);
+    }
+  }
   if (mouseQuad === 4) {
-    if (b4<255) {
-      b4=b4+15;
-      g4=g4-15;
-      fill(r4,g4,b4);
+    if (light === "off") {
+      if (b4<255) {
+        b4=b4+15;
+        g4=g4-15;
+        fill(r4,g4,b4);
+      }
+    }
+    else {
+      b4 = 255;
+      g4 = 0;
+    }
+  }
+  else {
+    if (light === "off") {
+      if (b4>0) {
+        b4=b4-15;
+        g4=g4+15;
+        fill(r4,g4,b4);
+      }
+    }
+    else {
+      b4 = 255;
+      g4 = 0;
+    }
   }
 }
+
 
 
 function mousePressed() {
@@ -112,6 +120,12 @@ function mousePressed() {
     else {
       b4 = 0;
       g4 = 255;
+    }
+    if (light === "off") {
+      light = "on";
+    }
+    else {
+      light = "off";
     }
   }
 }
