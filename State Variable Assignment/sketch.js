3f8089f9a8ede921e9c1a4bd2b297db1b177309d
logansosulski <47329522+logansosulski@users.noreply.github.com>
2019-03-08 11:01:51 -0600

    ghdh

--- State Variable Assignment/sketch.js
+++ State Variable Assignment/sketch.js
@@ -1,10 +1,11 @@
-// Project Title
-// Your Name
-// Date
+// State Variables Assignment: Rollovers
+// Logan Sosulskli
+// 2019/03/8
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// - I Implemented the Functionality from Canvas
 
+//Declaring Variables
 let mouseQuad = 2;
 let r1 = 255;
 let g1 = 255;
@@ -20,14 +21,17 @@ let g4 = 255;
 let b4 = 0;
 let light = "off";
 
+//Setting Up the Canvas
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
+//Drawing the Rectangles and Colouring them Appropriately
 function draw() {
   background(255);
   quadrentLocation();
   fade();
+  turnAllOn();
   fill(r1,g1,b1);
   rect(width / 2, 0, width / 2, height / 2);
   fill(r2,g2,b2);
@@ -38,6 +42,7 @@ function draw() {
   rect(width / 2, height / 2, width / 2, height / 2);
 }
 
+//Making the Rectangles Fade When Moused Over
 function fade() {
   if (mouseQuad === 1) {
     if (b1<255) {
@@ -109,10 +114,9 @@ function fade() {
   }
 }
 
-
-
+//When the Bottom Right Rectangle is Clicked Make it Turn On/Off
 function mousePressed() {
-  if (mouseX > width / 2 && mouseY > height / 2) {
+  if (mouseQuad === 4) {
     if (b4 === 0) {
       b4 = 255;
       g4 = 0;
@@ -130,6 +134,36 @@ function mousePressed() {
   }
 }
 
+//When the Top Left Corner is Clicked Make all the Rectangles Light Up
+//Until the Mouse Leaves the Area of the Rectangle
+function turnAllOn() {
+  if (mouseIsPressed === true) {
+    if (mouseQuad === 2) {
+      b1 = 255;
+      g1 = 0;
+      b2 = 255;
+      g2 = 0;
+      b3 = 255;
+      g3 = 0;
+      b4 = 255;
+      g4 = 0;
+    }
+    else {
+      b1 = 0;
+      g1 = 255;
+      b2 = 0;
+      g2 = 255;
+      b3 = 0;
+      g3 = 255;
+      if (light === "off") {
+        b4 = 0;
+        g4 = 255;
+      }
+    }
+  }
+}
+
+//Find What Quadrent the Mouse is in
 function quadrentLocation() {
   if (mouseX > width / 2) {
     if (mouseY < height / 2) {
