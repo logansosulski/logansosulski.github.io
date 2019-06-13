9200a90659647cd173e927cd8cb5471e1303e840
logansosulski <47329522+logansosulski@users.noreply.github.com>
2019-06-12 23:04:05 -0600

    sdfghytfd

--- Capstone Project/sketch.js
+++ Capstone Project/sketch.js
@@ -15,6 +15,7 @@ let selectedSquare;
 let mapArray = [];
 let units = [];
 let cities = [];
+let selectedUnits = [];
 
 function setup() {
   createCanvas(MAPWIDTH, MAPHEIGHT);
@@ -27,55 +28,17 @@ function draw() {
   displayObjects();
 }
 
-function displayObjects() {
-  for (let i = 0; i < cities.length; i++) {
-    cities[i].display();
-  }
-  for (let i = 0; i < units.length; i++) {
-    units[i].display();
-  }
-}
-
-function mouseClicked() {
-  cities.push(new City(2, 3, "blue"));
-  units.push(new Unit(3, 3, "blue"));
-  //find which square the mouse is over
-  findSquare();
-}
-
 function drawMap() {
   rectMode(CORNER);
   for (let x = 0; x < MAPWIDTH; x += TILESIZE) {
     for (let y = 0; y < MAPHEIGHT; y += TILESIZE) {
-      let mapData = [x,y];
+      let mapData = [x, y];
       mapArray.push(mapData);
     }
   }
   for (let i = 0; i < mapArray.length; i++) {
-    //fill(10,random(100,200),10);
     rect(mapArray[i][0], mapArray[i][1], TILESIZE, TILESIZE);
   }
-}
-
-function findSquare() {
-  rectMode(CORNER);
-  for (let i = 0; i < mapArray.length; i++) {
-    if (mouseX > mapArray[i][0] && mouseX < mapArray[i][0] + TILESIZE && mouseY > mapArray[i][1] && mouseY < mapArray[i][1] + TILESIZE) {
-      fill(0,255,0,50);
-      rect(mapArray[i][0], mapArray[i][1], TILESIZE, TILESIZE);
-      let mapData = [mapArray[i][0] - distX, mapArray[i][1] - distY];
-      print(mapData);
-      selectedSquare = mapData;
-    }
-    else {
-      fill(255);
-      rect(mapArray[i][0], mapArray[i][1], TILESIZE, TILESIZE);
-    }
-  } 
-}
-
-function highlightSelected() {
-  
 }
 
 function moveMap() {
@@ -186,6 +149,62 @@ function moveMap() {
   }
 }
 
+function displayObjects() {
+  for (let i = 0; i < cities.length; i++) {
+    cities[i].display();
+  }
+  for (let i = 0; i < units.length; i++) {
+    units[i].display();
+  }
+}
+
+function keyPressed() {
+  cities.push(new City(2, 3, "blue"));
+  units.push(new Unit(3, 3, "blue"));
+}
+
+function mousePressed() {
+  //find which square the mouse is over
+  if (mouseButton === LEFT) {
+    findSquare();
+    highlightSelected();
+  }
+  else if (mouseButton === CENTER) {
+    for (let i = 0; i < selectedUnits.length; i++) {
+      if (selectedUnits[i].info()[3] >= 5) {
+        selectedUnits[i].move();
+      }
+    }
+  }
+  print(selectedUnits);
+  //print(selectedUnits[0].info()[3]);
+  print(units);
+  print(selectedSquare);
+}
+
+function findSquare() {
+  for (let i = 0; i < mapArray.length; i++) {
+    if (mouseX > mapArray[i][0] && mouseX < mapArray[i][0] + TILESIZE && mouseY > mapArray[i][1] && mouseY < mapArray[i][1] + TILESIZE) {
+      //let mapData = [(mapArray[i][0] - distX) / TILESIZE, (mapArray[i][1] - distY) / TILESIZE];
+      let mapData = [mapArray[i][0], mapArray[i][1]];
+      print(mapData);
+      selectedSquare = mapData;
+    }
+  } 
+}
+
+function highlightSelected() {
+  selectedUnits = [];
+  fill(0, 255, 0, 70);
+  rectMode(CORNER);
+  rect(selectedSquare[0], selectedSquare[1], TILESIZE, TILESIZE);
+  for (let i = 0; i < units.length; i++) {
+    if (units[i].info()[0] === (selectedSquare[0] - distX) / TILESIZE && units[i].info()[1] === (selectedSquare[1] - distY) / TILESIZE) {
+      selectedUnits.push(units[i]);
+    }
+  }
+}
+
 class Unit {
   constructor(x_, y_, team_) {
     this.x = x_;
@@ -201,9 +220,27 @@ class Unit {
   }
 
 
-  //movement() {
-    
-  //}
+  move() {
+    if (mouseX > selectedSquare[0] + TILESIZE && mouseY > selectedSquare[1] && mouseY < selectedSquare[1] + TILESIZE) {
+      this.x += 1;
+      this.movement -= 5;
+    }
+
+    else if (mouseX < selectedSquare[0] && mouseY > selectedSquare[1] && mouseY < selectedSquare[1] + TILESIZE) {
+      this.x -= 1;
+      this.movement -= 5;
+    }
+
+    else if (mouseY > selectedSquare[1] + TILESIZE && mouseX > selectedSquare[0] && mouseX < selectedSquare[0] + TILESIZE) {
+      this.y += 1;
+      this.movement -= 5;
+    }
+
+    else if (mouseY < selectedSquare[1] && mouseX > selectedSquare[0] && mouseX < selectedSquare[0] + TILESIZE) {
+      this.y -= 1;
+      this.movement -= 5;
+    }
+  }
 
   display() {
     //imageMode(CENTER);
@@ -214,17 +251,14 @@ class Unit {
       fill(255,0,0);
     }
     ellipseMode(CENTER);
-    ellipse(this.x*TILESIZE-TILESIZE/2+distX, this.y*TILESIZE-TILESIZE/2+distY, this.size, this.size);
+    ellipse(this.x * TILESIZE + TILESIZE / 2 + distX, this.y * TILESIZE + TILESIZE / 2 + distY, this.size, this.size);
   }
 
-  location() {
-    let unitPos = [this.x,this.y];
-    return unitPos;
+  info() {
+    let unitInfo = [this.x, this.y, this.team, this.movement, this.health, this.pay];
+    return unitInfo;
   }
 
-  team() {
-    return this.team;
-  }
 
   defendCity() {
     this.health += 7;
@@ -240,7 +274,7 @@ class Unit {
 }
 
 class City {
-  constructor(x_,y_,team_) {
+  constructor(x_, y_, team_) {
     this.x = x_;
     this.y = y_;
     this.team = team_;
@@ -256,12 +290,12 @@ class City {
       fill(255,0,0,50);
     }
     rectMode(CENTER);
-    rect(this.x*TILESIZE-TILESIZE/2+distX, this.y*TILESIZE-TILESIZE/2+distY, this.size, this.size);
+    rect(this.x * TILESIZE + TILESIZE / 2 + distX, this.y * TILESIZE + TILESIZE / 2 + distY, this.size, this.size);
   }
 
   underAttack() {
     for (let i = 0; i < units.length; i++) { 
-      if (units[i].location()[0] === this.x && units[i].location()[1] === this.y && units[i].team() === this.team) {
+      if (units[i].info()[0] === this.x && units[i].info()[1] === this.y && units[i].info()[2] === this.team) {
         units[i].defend();
       }
     }
